48be83d3d9e320a26c6da793ee0287dcdabc69b1
Sheil <karaulov.z@mail.ru>
2024-04-01 19:40:47 +0300

    Небольшие правки

--- template/js/parallax.js
+++ template/js/parallax.js
@@ -1,7 +1,7 @@
 
 let bg = document.querySelector('.main__decoration');
 window.addEventListener('mousemove', function(e) {
-    let x = e.clientX / window.innerWidth * 0.2;
-    let y = e.clientY / window.innerHeight * 0.2;  
+    let x = e.clientX / window.innerWidth * 0.3;
+    let y = e.clientY / window.innerHeight * 0.3;  
     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
